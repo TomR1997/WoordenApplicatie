9c61bca072bf764f6d2537718c43db9709eefedd
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-03-07 14:31:50 +0100

    menu

--- HuffmanCodering/src/huffmancodering/HuffmanCodering.java
+++ HuffmanCodering/src/huffmancodering/HuffmanCodering.java
@@ -5,18 +5,38 @@
  */
 package huffmancodering;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Tomt
  */
 public class HuffmanCodering {
 
-    /**
-     * @param args the command line arguments
-     */
+    public HuffmanCodering()
+    {
+        menu();
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+        HuffmanCodering hc=  new HuffmanCodering();
     }
     
+    public void menu()
+    {
+       Scanner reader = new Scanner(System.in);
+       System.out.println("Type text:");
+
+        try {
+            String input="";
+
+            if (reader.hasNext()) {
+                input = reader.next();
+            }
+  
+        } catch (Exception e) {
+            e.printStackTrace();
+            menu();
+        }
+            
+    }
 }
